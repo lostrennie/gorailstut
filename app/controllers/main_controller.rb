96313c1f7237c335b7d60bf44e657023853dca86
jorman <lostrennie@gmail.com>
2022-08-15 12:22:01 -0400

    flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
     def index
+        # flash[:notice].now = "Logged in successfully"
+        # flash[:alert].now = "Invalid email or password"
     end
     
 end
